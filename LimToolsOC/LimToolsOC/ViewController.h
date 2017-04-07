b5f49934999a064385e3507434424d2d4ae12947
Liu <18355530563@163.com>
2017-04-07 11:19:39 +0800

    load oc project

---
+++ LimToolsOC/LimToolsOC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LimToolsOC
+//
+//  Created by Liu on 07/04/2017.
+//  Copyright © 2017 Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LimToolsOC/LimToolsOC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LimToolsOC
+//
+//  Created by Liu on 07/04/2017.
+//  Copyright © 2017 Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
